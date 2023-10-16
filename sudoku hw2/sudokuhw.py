55082bfcd73e3d7ac7b3d35af38d1b832f4221ef
Gleb <glebkuimov@gmail.com>
2023-10-16 14:25:10 +0300

    with menu

--- sudoku hw2/sudokuhw.py
+++ sudoku hw2/sudokuhw.py
@@ -1,7 +1,8 @@
 import random
 import pathlib
+import math
 
-grid = ("""53..7....
+grid = """53..7....
 6..195...
 .98....6.
 8...6...3
@@ -9,49 +10,55 @@ grid = ("""53..7....
 7...2...6
 .6....28.
 ...419..5
-....8..79""")
+....8..79"""
 
-def group(values: list, n: int):
-    lis1 = []
+lilgrid = """.2..
+4..1
+3...
+...2"""
+
+def group(values: list, n: int): #преобразовать одномерный список в двумерный
+    matrix = []
 
     for i in range(0, n):
-        lis1.append([])
+        matrix.append([])
         for j in range(0,n):
-            lis1[i].append(values[i*n + j])
-    return lis1
+            matrix[i].append(values[i*n + j])
+    return matrix
 
-
-def create_grid(puzzle: str):
+def create_grid(puzzle: str): #преобразовать строку в матрицу
     digits = [c for c in puzzle if c in "123456789."]
-    grid = group(digits, 9)
+    grid = group(digits, int(math.sqrt(len(puzzle.replace(" ", "").replace("\n", "")))))
     return grid
 
-def display(lis: list):
+def display(grid: list): #отобразить матрицу
     print("\n\n\n")
-    for i in range(len(lis)):
-        for j in range(len(lis[i])):
-            print(lis[i][j], end = "   ")
-            if (j + 1) % 3 == 0 and j % 8 != 0:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            print(grid[i][j], end = "   ")
+            if (j + 1) % int(math.sqrt(len(grid))) == 0 and j % (len(grid) - 1) != 0:
                 print("|", end = "   ")
-    print("\n")
-    if(i + 1) % 3 == 0 and i != len(lis)-1:
-        print("-----------------------------------------")
+        if(i + 1) % int(math.sqrt(len(grid))) == 0 and i != len(grid)-1:
+                print()
+                print("----------"* int(len(grid)/2))
+        else:
+            print("\n")
 
+display(create_grid(lilgrid))
 
-def getcol(puzzle: list, pos:tuple):
-    lis = []
+def getcol(puzzle: list, pos:tuple): # получить все значения из стобца указанной позиции
+    col = []
 
-    for i in range(0, len(puzzle)):
+    for i in range(len(puzzle)):
         if puzzle[i][pos[1]] != '.':
-            lis.append(puzzle[i][pos[1]])
-    return lis
+            col.append(puzzle[i][pos[1]])
+    return col
 
-
-def getrow(puzzle: list, pos:tuple):
+def getrow(puzzle: list, pos:tuple): # получить все значения из строки указанной позиции
     return [k for k in puzzle[pos[0]] if k != '.']
 
 
-def getblock(puzzle: list, pos:tuple): #ужас
+def getblock(puzzle: list, pos:tuple): #ужас # получить все значения из блока указанной позиции
     lis = []
     if pos[0] < 3:
         if pos[1] < 3:
@@ -111,55 +118,44 @@ def getblock(puzzle: list, pos:tuple): #ужас
                         lis.append(puzzle[i][j])
             return lis
 
-def find_empty_position(grid: list):
-    for i in range(0,len(grid)):
-        for j in range(0, len(grid[i])):
+def find_empty_position(grid: list): #найти первую по порядку свободную позицию в матрице
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == '.':
                 return (i,j)
     return 0
 
-def find_possible_values(grid: list, pos: tuple):
-    vals = set(str(i) for i in range(1,10))
+print(getblock(create_grid(lilgrid), (1,1)))
+
+def find_possible_values(grid: list, pos: tuple): #найти возмоные для данной свободной позиции значения
+    vals = set(str(i) for i in range(1, len(grid) + 1))
     col = set(i for i in getcol(grid, pos))
     row = set(i for i in getrow(grid, pos))
     block = set(i for i in getblock(grid, pos))
 
     return (vals - col) & (vals - row) & (vals - block)
 
-
-
-
-def solve(grid1: list):
-    pos = find_empty_position(grid1)
-    print(pos)
-    if not pos:
-        return grid1
-    vals = find_possible_values(grid1, pos)
-    print(vals)
-    for val in vals:
-        grid1[pos[0]][pos[1]] = val
-        solve(grid1)
-    display(grid1)
-
+def solve(grid1: list): #решить судоку
+    print("\n\nНЕ РАБОТАЕТ!")
     pass
-print(solve(create_grid(grid)))
 
-def check_solution(grid: list):
-    a = set(str(i) for i in range(1,10))
-    for i in range(0, 9):
-        for j in range(0, 9):
-            if not set(getcol(grid ,(i,j))) == a or not set(getrow(grid ,(i,j))) == a or not set(getblock(grid ,(i,j))) == a:
+
+def check_solution(grid: list): #проверить решение
+    alldigits = set(str(i) for i in range(1, len(grid) + 1))
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if not set(getcol(grid ,(i,j))) == alldigits or not set(getrow(grid ,(i,j))) == alldigits or not set(getblock(grid ,(i,j))) == alldigits:
                 print("---------INCORRECT SOLUTION OF THE GIVEN SUDOKU GRID--------")
                 return
     print("---------SUDOKU WAS SOLVED CORRECTLY--------")
 
-def generate_grid(n: int):
+def generate_grid(n: int): #сгенеририровать новую нерешенную матрицу #работает неправильно, поскольку для правильной генерации необходима написанная функция solve()
     trugrid = create_grid('.' * 81)
-    used = [(random.randint(0, 8), random.randint(0,8))]
+    used = [(random.randint(0, len(grid) - 1), random.randint(0, len(grid) - 1))]
     p = 0
     f = n
     while p < n:
-        pos1 = (random.randint(0, 8), random.randint(0,8))
+        pos1 = (random.randint(0, len(grid) - 1), random.randint(0, len(grid) - 1))
         if used[p] != (pos1):
             used.append(pos1)
             p = p + 1
@@ -175,10 +171,36 @@ def generate_grid(n: int):
         print("количество пропущенных позиций = ".upper(), n - f)
     return trugrid
 
-def read_sudoku(path: str):
+def read_sudoku(path: str): # прочитать матрицу из указанного файла
     """ Прочитать Судоку из указанного файла """
     path = pathlib.Path(path)
     with path.open() as f:
         puzzle = f.read()
     return create_grid(puzzle)
 
+def menu():
+    ext = 1
+    while(ext):
+        print("\n\n\n\n--------Домашнее задание 2: Судоку----------\n\n\n\n")
+        print("1 - Прочитать матрицу из файла в данной директории \n2 - Отобразить матрицу \n3 - Решить судоку(не работает)")
+        ans = input("\n\n*******ДЛЯ ВЫХОДА ВВЕДИТЕ 0*******\n")
+        match ans:
+            case '1':
+                name = input("\nВведите название файла: ")
+                try:
+                    matrix = read_sudoku(name)
+                except FileNotFoundError:
+                    print("\n!!!!!!!Введите корректное название файла!!!!!!!")
+            case '2':
+                display(matrix)
+            case '3': 
+                solve(matrix)
+            case '0':
+                ext = 0
+                break
+            case _:
+                print("\nВведите корректный ответ")
+
+if __name__ == "__main__":
+    menu()
+    pass
